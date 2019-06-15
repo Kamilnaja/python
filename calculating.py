2362fe49d06ba65fdc31ca09696e200efcde77ff
najak <kamilnaja@gmail.com>
2019-06-15 11:27:55 +0200

    dividing

--- calculating.py
+++ calculating.py
@@ -1,84 +1,92 @@
-
-nums = [0, 'operation', 0]
+nums = [0, "operation", 0]
+operators = ['*', '/', '+', '-']
 
 def setupValues(val, numVal):
     if val == numVal:
         nums[2] = val
-    if isinstance(val, str):
+    if val in operators:
         nums[0] = numVal
         nums[1] = val
-    
+
+def printParsed():
+    return ','.join(str(e) for e in nums).replace(',', '')
 
 def zero(val=0):
-    return setupValues(val, 0)
-
+    setupValues(val, 0)
+    if val in operators:
+        return eval(printParsed())
 
 def one(val=1):
     setupValues(val, 1)
-    
+    if val in operators:
+        return eval(printParsed())
+
+
 def two(val=2):
     setupValues(val, 2)
+    if val in operators:
+        return eval(printParsed())
+
 
 def three(val=3):
     setupValues(val, 3)
+    if val in operators:
+        return eval(printParsed())
 
-def four(val=3):
+
+def four(val=4):
     setupValues(val, 4)
+    if val in operators:
+        return eval(printParsed())
+
 
 def five(val=5):
     setupValues(val, 5)
+    if val in operators:
+        return eval(printParsed())
+
 
 def six(val=6):
     setupValues(val, 6)
+    if val in operators:
+        return eval(printParsed())
+
 
 def seven(val=7):
     setupValues(val, 7)
+    if val in operators:
+        return eval(printParsed())
+
 
 def eight(val=8):
     setupValues(val, 8)
+    if val in operators:
+        return eval(printParsed())
+
 
 def nine(val=9):
     setupValues(val, 9)
+    if val in operators:
+        return eval(printParsed())
+
 
 def plus(val=0):
-    return '+'
+    return "+"
 
 def minus(val):
     return "-"
 
+
 def times(val):
-    return '*'
+    return "*"
+
 
 def divided_by(val):
     return "/"
 
-# one(plus(one()))
-# print(nums[0], nums[1], nums[2], 'one one')
-# strToCalculate = ','.join([str(x) for x in nums]).replace(',', '')
-# print(eval(strToCalculate))
-
-
-# zero(plus(zero()))
-# print(nums[0], nums[1], nums[2], 'zero zero')
-# strToCalculate = ','.join([str(x) for x in nums]).replace(',', '')
-# print(eval(strToCalculate))
-
-# two(plus(one()))
-# print(nums[0], nums[1], nums[2], 'two one')
-# strToCalculate = ','.join([str(x) for x in nums]).replace(',', '')
-# print(eval(strToCalculate))
-
-
-# two(plus(three()))
-# print(nums[0], nums[1], nums[2], 'two three')
-# strToCalculate = ','.join([str(x) for x in nums]).replace(',', '')
-# print(eval(strToCalculate))
-
-
-# nine(divided_by(five()))
-# print(nums[0], nums[1], nums[2], 'nine five')
-# strToCalculate = ','.join([str(x) for x in nums]).replace(',', '')
-# print(eval(strToCalculate))
-
-# seven(times(five()))
 print(zero(plus(zero())))
+print(zero(plus(one())))
+print(seven(times(five())))
+print(four(plus(nine())))
+print(eight(minus(three())))
+print(seven(divided_by(nine())))
