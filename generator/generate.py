aba038bf74359651bb1aa938a6a6bbfda55ba8dc
najak <kamilnaja@gmail.com>
2019-06-16 14:08:52 +0200

    new fns

--- generator/generate.py
+++ generator/generate.py
@@ -1,16 +1,39 @@
-from generator import generateRandomWord
+import random
+from enum import Enum
+
+
+class State(Enum):
+    NOT_FOUND = 0  # no match between searched and generated
+    FOUND = 1  # found one or more
+    ALL_EQUALS = 2  # all ok
+    STR_GENERATED = 3
+    NONE_EQUAL = 4  # none match
+    PARTIALY_EQUAL = 5  # some match
+
 
 searchedString = "lorem"
-states = ("NOT_FOUND", "ONE_OK", "ALL_OK")
-state = ""
-
-if state == states[0]:  # none found
-    print("not found")
-elif state == states[1]:  # one ok
-    print("one ok, searching")
-elif state == states[2]:  # all ok
-    print("we have found the searched string!")
+state = State.NONE_EQUAL
+currentSearchedLen = len(searchedString)
+tempRandomStr = ""
 
 
+def generateRandomWord(searchedLen=len(searchedString)):
+    letters = []
+    for x in range(searchedLen):
+        randomLetter = random.randint(65, 90)
+        letters.append(chr(randomLetter).lower())
+    return ','.join(letters).replace(',', '')
 
-generateRandomWord(len(searchedString))
+
+def compareRandomWithSearched(random, searched):
+    print(random)
+    print(searched)
+
+
+if state == State.NONE_EQUAL:
+    tempRandomStr = generateRandomWord()  # generate without params
+    compareRandomWithSearched(searchedString, tempRandomStr)
+elif state == State.ALL_EQUALS:  # generate with
+    generateRandomWord(5)
+elif state == State.ALL_EQUALS:
+    print("We have succesfully generated your string!")
--- generator/generator.py
+++
@@ -1,6 +0,0 @@
-import random
-
-def generateRandomWord(searchedLen):
-    for x in range(searchedLen):
-        randomLetter = random.randint(65, 90)
-        print(chr(randomLetter).lower())
