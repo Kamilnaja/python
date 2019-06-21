003d38b55d6bb735c45dab506dff083395c3e757
najak <kamilnaja@gmail.com>
2019-06-21 17:45:12 +0200

    handle space #1

--- generator/generator.py
+++ generator/generator.py
@@ -2,14 +2,19 @@ import random
 
 def generateRandomWord(searchedLen):
     letters = []
-    for x in range(searchedLen):
-        randomLetter = random.randrange(97, 122)
+    for _ in range(searchedLen):
+        aciiStart = 97
+        asciiEnd = 122 + 1 # because of exclusive randrange
+        randomLetter = random.randrange(aciiStart, asciiEnd + 1) 
+        # replace 124 with space
+        if randomLetter == 123:
+            randomLetter = 32
         letters.append(chr(randomLetter))
     return ",".join(letters).replace(",", "")
 
 
 def generateResponseTable(searched):
     resTable = []
-    for x in searched:
+    for _ in searched:
         resTable.append('_')
     return ''.join(resTable)
