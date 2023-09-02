88d63e36acf548e69f514a23646c4d66bf6b04bf
KAMIL.NAJA <KAMIL.NAJA@EXTERNAL.NEXIGROUP.COM>
2023-09-02 12:16:14 +0200

    Preventing empty folder deletion

--- text-classification.py
+++ text-classification.py
@@ -13,4 +13,22 @@ from tensorflow.keras import losses
 #                                   untar=True, cache_dir='.',
 #                                   cache_subdir='')
 dataset_dir = '.\\aclImdb'
-print(os.listdir(dataset_dir))
+
+train_dir = os.path.join(dataset_dir, 'train')
+
+remove_dir = os.path.join(train_dir, 'unsup')
+if (os.path.exists(remove_dir)):
+    shutil.rmtree(remove_dir)
+else:
+    print("Dir not found")
+
+batch_size = 32
+seed = 42
+
+raw_train_ds = tf.keras.preprocessing.text_dataset_from_directory(
+    'aclImdb/train',
+    batch_size=batch_size,
+    validation_split=0.2,
+    subset='training',
+    seed=seed
+)
