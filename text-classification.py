5e3880f9980c7a17abd03861906d6aa8ca7a1801
KAMIL.NAJA <KAMIL.NAJA@EXTERNAL.NEXIGROUP.COM>
2023-09-02 12:04:59 +0200

    getting initial data for text classification

--- text-classification.py
+++ text-classification.py
@@ -8,9 +8,9 @@ import tensorflow as tf
 from tensorflow.keras import layers
 from tensorflow.keras import losses
 
-url = "https://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz"
-dataset = tf.keras.utils.get_file("aclImdb_v1.tar.gz", url,
-                                  untar=True, cache_dir='.',
-                                  cache_subdir='')
-dataset_dir = os.path.join(os.path.dirname(dataset), 'aclImdb')
+# url = "https://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz"
+# dataset = tf.keras.utils.get_file("aclImdb_v1.tar.gz", url,
+#                                   untar=True, cache_dir='.',
+#                                   cache_subdir='')
+dataset_dir = '.\\aclImdb'
 print(os.listdir(dataset_dir))
