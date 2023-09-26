9f34f4bd9f330d2f5a2937fcdcc9006956a7909b
kamilnaja <kamilnaja@gmail.com>
2023-09-26 21:04:23 +0200

    configuring tensorboard

---
+++ cnn.py
@@ -0,0 +1,6 @@
+import numpy as np
+import tensorflow as tf
+
+mnist = tf.keras.datasets.mnist
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+print(train_images.shape)
