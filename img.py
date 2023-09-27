107a554704b8e766e3996094cc9edaf2b274c9d0
KAMIL.NAJA <KAMIL.NAJA@EXTERNAL.NEXIGROUP.COM>
2023-09-27 19:53:48 +0200

    mat plot lib

--- img.py
+++ img.py
@@ -5,3 +5,10 @@ import matplotlib.pyplot as plt
 
 fashion_mnist = tf.keras.datasets.fashion_mnist
 (train_images, train_labels), (test_images) = fashion_mnist.load_data()
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+
+train_images = train_images / 255.0
+
+test_images = test_images / 255.0
