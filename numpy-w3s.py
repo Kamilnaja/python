213ec1a4af60f766ff245eaacd9ddda92fa722e9
kamilnaja <kamilnaja@gmail.com>
2023-10-04 21:50:12 +0200

    np learn

--- numpy-w3s.py
+++ numpy-w3s.py
@@ -1,4 +1,5 @@
 import numpy as np
+
 arr1 = np.array(42)
 arr2 = np.array([1, 2, 3, 4, 5])
 arr3 = np.array([[1, 2, 3], [4, 5, 6]])
@@ -10,5 +11,8 @@ arr = np.array([1, 2, 3, 4, 5, 6, 7])
 x = arr.copy()
 arr[0] = 42
 
-print(arr)
-print(x)
+arr = np.array([1.1, 2.2, 3.3, 4.5, 5.1])
+newarr = arr.astype('i')
+
+print(newarr)
+print(newarr.dtype)
