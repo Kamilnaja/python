753e92637386ebd51db5585048223de77eb4e2d8
kamilnaja <kamilnaja@gmail.com>
2024-01-07 11:58:04 +0100

    proceeding with housing example

---
+++ didl/add_to_class.py
@@ -0,0 +1,15 @@
+def add_to_class (Class): #@save
+  def wrapper(obj):
+    setattr(Class, obj.__name__, obj)
+  return wrapper
+
+class A:
+  def __init__(self):
+    self.b = 1
+a = A()
+
+@add_to_class(A)
+def do(self):
+  print('Class attribute "b" is', self.b)
+
+a.do()
