20e5a4101e8210aa7db3741e7195fa3db3a9161c
kamilnaja <kamilnaja@gmail.com>
2024-02-10 09:18:03 +0100

    progressing with configuration

--- didl/add_to_class.py
+++ didl/add_to_class.py
@@ -1,15 +1,17 @@
-def add_to_class (Class): #@save
-  def wrapper(obj):
-    setattr(Class, obj.__name__, obj)
-  return wrapper
+def add_to_class(Class):  # @save
+    def wrapper(obj):
+        setattr(Class, obj.__name__, obj)
+    return wrapper
+
 
 class A:
-  def __init__(self):
-    self.b = 1
+    def __init__(self):
+        self.b = 1
+
+
 a = A()
+
 
 @add_to_class(A)
 def do(self):
-  print('Class attribute "b" is', self.b)
-
-a.do()
+    print('Class attribute "b" is', self.b)
--- didl/reg.py
+++
@@ -1,5 +0,0 @@
-%matplotlib inline
-import math
-import time
-import numpy as np
-import tensorflow as tf
